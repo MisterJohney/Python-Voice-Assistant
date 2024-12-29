8e455f44822138f99c6ddecc183b831006d02394
MisterJohney <johneylikestoplay@gmail.com>
2024-12-29 11:37:31 +0200

    Refactored main.py file (Needs testing if it works)

--- src/main.py
+++ src/main.py
@@ -1,48 +1,61 @@
+import logging
+
 import whisper
 from langchain_ollama import OllamaLLM
 import edge_tts
 from playsound3 import playsound
 
-def transcribe():
-    INPUT_FILE = "../tests/recording.wav"
-    OUTPUT_FILE = "../tests/input_text.txt"
+INPUT_FILE = "../tests/recording.wav"
+OUTPUT_FILE = "../tests/compleation.mp3"
+
+def transcribe(input_audio_file):
+    # OUTPUT_FILE = "../tests/input_text.txt"
 
     model = whisper.load_model("base")
-    result = model.transcribe(INPUT_FILE)
+    result = model.transcribe(input_audio_file)
     # print(result["text"])
-    with open(OUTPUT_FILE, "w") as f:
-        f.write(result["text"])
+    # with open(OUTPUT_FILE, "w") as f:
+    #     f.write(result["text"])
+    return result["text"]
 
 
-def prompt():
+def prompt(input_text):
     model = OllamaLLM(model="llama3.2")
-    INPUT_FILE = "../tests/input_text.txt"
-    OUTPUT_FILE = "../tests/output_text.txt"
+    # INPUT_FILE = "../tests/input_text.txt"
+    # OUTPUT_FILE = "../tests/output_text.txt"
     
-    with open(INPUT_FILE, "r") as f:
-        text = f.read()
-    
-    result = model.invoke(input=text)
+    # with open(INPUT_FILE, "r") as f:
+    #     text = f.read()
+    #
+    # result = model.invoke(input=text)
+    result = model.invoke(input=input_text)
     
     # print(result)
     
-    with open(OUTPUT_FILE, "w") as f:
-        f.write(result)
+    # with open(OUTPUT_FILE, "w") as f:
+    #     f.write(result)
+    return result
 
-def tts():
-    with open("../tests/output_text.txt", "r") as f:
-        text = f.read()
+def tts(input_text):
+    # with open("../tests/output_text.txt", "r") as f:
+    #     text = f.read()
 
     VOICE = "en-GB-SoniaNeural"
-    OUTPUT_FILE = "../tests/compleation.mp3"
-    communicate = edge_tts.Communicate(text, VOICE)
+    # communicate = edge_tts.Communicate(text, VOICE)
+    communicate = edge_tts.Communicate(input_text, VOICE)
     communicate.save_sync(OUTPUT_FILE)
 
-print("transcribing")
-transcribe()
-print("prompting")
-prompt()
-print("ttsing")
-tts()
-print("playing")
-playsound("../tests/compleation.mp3")
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("transcribing")
+    recorded_text = transcribe(INPUT_FILE)
+
+    logging.info("prompting")
+    answer_text = prompt(recorded_text)
+
+    logging.info("ttsing")
+    tts(answer_text)
+
+    logging.info("playing")
+    playsound(OUTPUT_FILE)
